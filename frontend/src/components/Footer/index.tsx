4841c242f5bab5171e5533f19bf612fd95cc3600
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2021-05-04 11:41:31 -0300

    Referencia atualizada

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <footer className="footer mt-auto py-3 bg-dark">
                     <div className="container">
-                        <p className="text-light">App desenvolvido por <a href="https://github.com/mauricioqvp" target="_blank" rel="noreferrer">Nélio Alves</a></p>
+                        <p className="text-light">App desenvolvido por <a href="https://github.com/mauricioqvp" target="_blank" rel="noreferrer">Maurício Quaranta</a></p>
                         <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
                     </div>
