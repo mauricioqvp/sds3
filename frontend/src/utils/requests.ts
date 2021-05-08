919eb3e5fc15a672ccbf723ec673149cf3ff4ad5
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2021-05-07 21:44:11 -0300

    Address altered.

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://mauricioqvp-sds3.herokuapp.com/';
