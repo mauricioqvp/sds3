0e7e38ca69f8c41d78502ea2fe533e3474a4ec53
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2021-05-07 22:44:18 -0300

    BaseURL altered2

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://mauricioqvp-sds3.herokuapp.com/';
+export const BASE_URL = 'https://mauricioqvp-sds3.herokuapp.com';
 
 //process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
